e0fcd1cabb4e1c3a2077d6c08e7dbcc567175d34
bruceliator <alexandr.tsibulko@gmail.com>
2014-11-22 20:35:23 +0200

    try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 TaskManager::Application.configure do
 # Settings specified here will take precedence over those in config/application.rb.
-Rails.application.routes.default_url_options[:host] = 'task-maneger.herokuapp.com'
 config.action_mailer.default_url_options = { :host => 'task-maneger.herokuapp.com }
+TaskManager::Application.routes.default_url_options[:host] = 'task-maneger.herokuapp.com'
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
