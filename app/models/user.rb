f937521a48372f9ec49f11cd567430eb3981a01d
bruceliator <alexandr.tsibulko@gmail.com>
2014-11-22 17:48:51 +0200

    try to fix production

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable 
 
- validates_presence_of :first_name
- validates_presence_of :last_name
+ #validates_presence_of :first_name
+ #validates_presence_of :last_name
 end
