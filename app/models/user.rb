b346266c467deb3809f063c365144adc4037a023
bruceliator <alexandr.tsibulko@gmail.com>
2014-11-22 18:02:25 +0200

    try

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable 
+         :recoverable, :rememberable, :trackable, :validatable
 
  #validates_presence_of :first_name
  #validates_presence_of :last_name
