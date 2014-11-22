a1aa656fb8575acaaa95bfee392b1a2c52bcf698
bruceliator <alexandr.tsibulko@gmail.com>
2014-11-22 19:52:42 +0200

    try to fix registration

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20141120151030) do
   end
 
   create_table "users", force: true do |t|
+    t.string   "first_name",             default: "", null: false
+    t.string   "last_name",              default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -41,8 +43,6 @@ ActiveRecord::Schema.define(version: 20141120151030) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
