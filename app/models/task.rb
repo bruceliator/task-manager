c2efa625ea43eb0b1e452150fe0c648b0c96e33f
bruceliator <alexandr.tsibulko@gmail.com>
2014-11-30 14:26:45 +0200

    Add future date validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ActiveRecord::Base
  belongs_to :user
  validates :title, :description, :due_date, :priority, :user_id, :presence => true
+ validate :task_set_in_the_future
+ 
+ 
+ def task_set_in_the_future
+    if due_date <= DateTime.current
+      errors.add(:task, "Please Choose A Future Date & Time")
+    end
+  end
+  
 end
